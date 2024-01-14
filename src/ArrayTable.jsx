b7b9ccb9cb6a0475feaa38e99154fe705a8243e5
DasolXandzo <59554571+DasolXandzo@users.noreply.github.com>
2024-01-14 07:56:40 +0300

    global (not) update

--- src/ArrayTable.jsx
+++ src/ArrayTable.jsx
@@ -1,35 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
+import cl from './ArrayTable.module.css';
 
-const ArrayTable = ({ dataArray }) => {
-    console.log(dataArray);
-  const renderRow = (row, index) => (
-    <tr key={index}>
-      <td style={{paddingLeft: (row.Depth) * 20}}>{row.Name}</td>
-      <td style={{textAlign: "center"}}>{row.Result}</td>
-      <td style={{textAlign: "center"}}>{row.Depth}</td>
-      {/* Добавьте дополнительные столбцы, если необходимо */}
-    </tr>
-  );
+const ArrayTable = ({ treeData }) => {
+  const [collapsedRows, setCollapsedRows] = useState([]);
+
+  const toggleCollapse = (index) => {
+    setCollapsedRows((prevCollapsedRows) => {
+      if (prevCollapsedRows.includes(index)) {
+        return prevCollapsedRows.filter((i) => i !== index);
+      } else {
+        return [...prevCollapsedRows, index];
+      }
+    });
+  };
+
+  const renderRows = (nodes, parentIndex = '') => {
+    return nodes.map((node, index) => (
+      <React.Fragment key={parentIndex + '-' + index}>
+        <tr className={cl.tr_body}>
+          <td
+            style={{
+              paddingLeft: node.depth * 20,
+              display: 'flex',
+              alignItems: 'center',
+              width: `${604 - node.depth * 20}px`,
+            }}
+            //onClick={() => toggleCollapse(parentIndex + '-' + index)}
+          >
+            {/* если есть дочерние элементы, то добавляем активную кнопку для эффекта коллапса */}
+            {node.children.length != 0 && (
+              <button
+              className={`${"collapse-btn"} ${cl.collapse_btn}`}
+              onClick={() => toggleCollapse(parentIndex + '-' + index)}
+              >
+                {collapsedRows.includes(parentIndex + '-' + index) ? '+' : '-'}
+              </button>
+            )}
+
+            {/* если нет дочерних элементов, то добавляем ёбанный костыль - кнопку-заглушку, чтобы отступы не ехали */}
+            {node.children.length == 0 && (<button className={cl.fake_btn}>+</button>)}
+            {node.name}
+          </td>
+          <td style={{ textAlign: 'center' }}>{node.result}</td>
+          <td style={{ textAlign: 'center' }}>{node.depth}</td>
+        </tr>
+        {!collapsedRows.includes(parentIndex + '-' + index) &&
+          node.children &&
+          node.children.length > 0 && (
+            renderRows(node.children, parentIndex + '-' + index)
+          )}
+      </React.Fragment>
+    ));
+  };
 
   return (
-    <table style={{ width: '100%' }}>
+    <table className="table table-sm table-hover" style={{ width: '100%' }}>
       <colgroup>
-        <col span="1" style={{ width: '50%' }}></col>
-        <col span="1" style={{ width: '25%' }}></col>
-        <col span="1" style={{ width: '25%' }}></col>
-        {/* Добавьте дополнительные столбцы, если необходимо */}
+        <col span="1" style={{ width: '64%' }}></col>
+        <col span="1" style={{ width: '13%' }}></col>
+        <col span="1" style={{ width: '13%' }}></col>
       </colgroup>
-      <thead>
+      <thead
+        style={{
+          backgroundColor: '#000000',
+          color: '#ffffff',
+        }}
+      >
         <tr>
           <th>Name</th>
           <th>Result</th>
           <th>Depth</th>
-          {/* Добавьте заголовки для дополнительных столбцов, если необходимо */}
         </tr>
       </thead>
-      <tbody>
-        {dataArray.map(renderRow)}
-      </tbody>
+      <tbody>{renderRows(treeData)}</tbody>
     </table>
   );
 };
--- src/DialigWindow.jsx
+++ src/DialigWindow.jsx
@@ -7,7 +7,7 @@ import ArrayTable from './ArrayTable';
 export default function DialigWindow(props) {
 
   // add some styles for Dialog window
-  const bodyStyle = {fontSize: "16px"};
+  const bodyStyle = {fontSize: "13px"};
   const block = {marginBottom: "15px"};
   const hedlineStyle = {fontSize: "120%", fontWeight: "bold"};
   const treeHedlineStyle = {fontSize: "140%", fontWeight: "bold"};
@@ -28,24 +28,69 @@ export default function DialigWindow(props) {
     }
   }, []);
 
+
+// convert array to tree
+//----------------------------------------------------------
+  class Node {
+    constructor(name, result, depth) {
+      this.name = name;
+      this.result = result;
+      this.depth = depth;
+      this.children = [];
+    }
+  }
+  
+  function buildTree(inputArray) {
+    const root = new Node("Root", 0, -1);
+    const stack = [root];
+  
+    for (const item of inputArray) {
+      const node = new Node(item.name, item.result, item.depth);
+  
+      while (stack.length > 1 && stack[stack.length - 1].depth >= node.depth) {
+        stack.pop();
+      }
+  
+      stack[stack.length - 1].children.push(node);
+      stack.push(node);
+    }
+  
+    return root.children;
+  }
+
+  const inputArray = []
+  for(var i=0; i<array.length; i++){
+    inputArray.push({name: array[i].Name, result: array[i].Result, depth: array[i].Depth, children: []})
+  }
+
+  console.log(inputArray);
+
+  const tree = buildTree(inputArray);
+
+  console.log(tree);
+//----------------------------------------------------------
+
+
   return (
     <div style={bodyStyle}>
 
       <div style={block}>
-        <div style={hedlineStyle}>Выбранная формула:</div>
-        <div>{props.lettersFormula.replace(/\@/g, "+")}</div>
-      </div>
-
-      <div style={block}>
-        <div style={hedlineStyle}>Формула с подставленными значениями:</div>
-        <div>{props.valuesFormula}</div>
-      </div>
-
-      <div style={block}>
-        <div style={treeHedlineStyle}>Дерево функции</div>
-        <TreeComponent data={array} />
-        <ArrayTable dataArray={array} />
+        {/*<div style={treeHedlineStyle}>Дерево функции</div>*/}
+        {/*<TreeComponent data={array} />*/}
+        <ArrayTable treeData={tree} />
         {/*<Tree tree={array} />*/}
+      </div>
+
+      <div>
+        <div style={block}>
+          <div style={hedlineStyle}>Выбранная формула:</div>
+          <div>{props.lettersFormula.replace(/\ /g, "+")}</div>
+        </div>
+
+        <div style={block}>
+          <div style={hedlineStyle}>Формула с подставленными значениями:</div>
+          <div>{props.valuesFormula}</div>
+        </div>
       </div>
 
     </div>
